f02f64133e5420723a8f719dacf607f17dcefaba
Javier Gonzalez <javiergonzalez01@aragon.unam.mx>
2022-06-10 13:37:43 -0500

    Modificaciones funcionales

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
     <link href="assets/Recursos/estiloIndex.css" rel="stylesheet" type="text/css"> 
     <link href="https://fonts.googleapis.com/css2?family=Kumar+One+Outline&amp;display=swap" rel="stylesheet">
     <script src="https://kit.fontawesome.com/6aaca85f05.js" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     <title>Document</title>
 </head>
 <body>
@@ -21,22 +22,19 @@
                 <div class="collapse navbar-collapse" id="navbarNavDropdown">
                     <ul class="navbar-nav">
                         <li class="nav-item">
-                            <a class="nav-link" href="#" style="color: #fff;">Algo</a>
+                            <a class="nav-link" href="formulario_registrar.php" style="color: #fff;">Registrar Nuevos Usuarios</a>
                         </li>
                         <li class="nav-item texto">
-                            <a class="nav-link" style="color: #fff;" href="">Algo</a>
+                            <a class="nav-link" style="color: #fff;" href="listar.php">Editar Registros</a>
                         </li>
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle"  style="color: #fff;" href="" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Algo
+                            Más Opciones
                             </a>
                             <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                                    <a class="dropdown-item" href="">Algo</a>
-                                    <a class="dropdown-item" href="">Algo</a>
-                                    <a class="dropdown-item" href="">Algo</a>
-                                    <a class="dropdown-item" href="">Algo</a>
-                                    <a class="dropdown-item" href="">Algo</a>
-                                    <a class="dropdown-item" href="">Algo</a>
+                                    <a class="dropdown-item" href="listar.php">Ver Usuarios</a>
+                                    <a class="dropdown-item" href="listar.php">Modificar Usuarios</a>
+                                    <a class="dropdown-item" href="listar.php">Eliminar Usuarios</a>
                             </div>
                         </li>
                     </ul>
@@ -77,6 +75,8 @@
 
 
     <script src="http://localhost/Proyecto-OACC/assets/bootstrap/js/bootstrap.min.js"></script>
-    
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 </body>
 </html>
